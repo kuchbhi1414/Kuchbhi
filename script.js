5550bda489416657a5b563172e44a658c6d5ecef
kuchbhi1414 <mauryaraj1414@gmail.com>
2025-05-07 20:42:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@
         setInterval(createHeart, 300);
     });
 document.getElementById("startButton").addEventListener("click", () => {
-  const audio = new Audio("birthday_music(1).mp3");
+  const audio = new Audio("birthday_music(2).mp3");
   audio.loop = true;
   audio.play().catch(error => {
     console.log("Autoplay prevented:", error);
